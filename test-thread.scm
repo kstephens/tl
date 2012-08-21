de3a351344be8fc5e89be3cfdecdc62e6f6eeaa6
Kurt Stephens <ks.github@kurtstephens.com>
2012-08-21 01:38:22 -0500

    Added tl_pthread_self().

--- test-thread.scm
+++ test-thread.scm
@@ -8,10 +8,10 @@
     ;(&debug)
     'OK!
     ))
-(newline)(display "In main thread")(newline)(io-flush)
+(newline)(display "In main thread.  ")(newline)(io-flush)
 (set! new-thread (tl_pthread_create thread_proc nil))
 ; (write "Waiting for other thread to finish")(newline)
 (define new-thread-result (tl_pthread_join new-thread))
-(write "Back in main thread")(newline)
-(write "new-thread-result = ")(write new-thread-result)(newline)
+(display "Back in main thread.  ")(newline)(io-flush)
+(display "new-thread-result = ")(write new-thread-result)(newline)
 
