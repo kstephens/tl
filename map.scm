9598d155513c12d1179cb83a7c4f5b3d0d052dea
Kurt Stephens <ks.github@kurtstephens.com>
2012-08-26 20:27:10 -0500

    Use (define (n . a) . b) syntax.

--- map.scm
+++ map.scm
@@ -1,16 +1,14 @@
 (define <map> (make-type "map"))
-(define map-new (lambda () (cons '() '())))
-(define map-lookup (lambda (self key) (assq key (car self))))
-(define map-get
-  (lambda (self key)
-    (let ((slot (map-lookup self key)))
-      (if slot (cdr slot) '()))))
-(define map-set
-  (lambda (self key value)
-    (let ((slot (map-lookup self key)))
-      (if slot
-        (set-cdr! slot value)
-        (set-car! self (cons (cons key value) (car self)))
-        ))))
+(define (map-new) (cons '() '()))
+(define (map-lookup self key) (assq key (car self)))
+(define (map-get self key)
+  (let ((slot (map-lookup self key)))
+    (if slot (cdr slot) '())))
+(define (map-set self key value)
+  (let ((slot (map-lookup self key)))
+    (if slot
+      (set-cdr! slot value)
+      (set-car! self (cons (cons key value) (car self)))
+      )))
 
 'ok
