8f8a1689e52880722b1647e01f2faf4cacf0c694
Kurt Stephens <ks.github@kurtstephens.com>
2012-08-26 20:27:10 -0500

    Use (define (n . a) . b) syntax.

--- macro-expander.scm
+++ macro-expander.scm
@@ -12,59 +12,43 @@
 (define *macro-expand-trace* #f)
 (define <macro-environment> (make-type "macro-environment"))
 (let ((id 0))
-  (define macro-environment-new
-    (lambda ()
-      (set! id (+ id 1))
-      ;; (tl_set_type 
-        (vector '<struct> #f #f id #f (map-new))
-      ;; <vector>) ;; fixme
-      )))
-(define macro-environment-id
-  (lambda (self)
-    (vector-ref self 3)))
-(define macro-environment-parent
-  (lambda (self)
-    (vector-ref self 4)))
-(define macro-environment-parent=
-  (lambda (self v)
-    (vector-set! self 4 v) self))
-(define macro-environment-bindings
-  (lambda (self)
-    (vector-ref self 5)))
-(define macro-environment-bindings=
-  (lambda (self v)
-    (vector-set! self 5 v) self))
-(define macro-environment-write 
-  (lambda (self port op)
-    (display "#<macro-environment " port)
-    (write (macro-environment-id self) port)
-    (display" >" port)))
-; (%object-writer <macro-environment> macro-environment-write)
-(define macro-environment-set-transformer
-  (lambda (self symbol transformer)
-    (map-set (macro-environment-bindings self) symbol transformer)
-    ;; (display "\n\n  ### set-macro ")(write symbol)(write transformer)(display "\n\n")
-    self
+  (define (macro-environment-new)
+    (set! id (+ id 1))
+    ;; (tl_set_type 
+    (vector '<struct> #f #f id #f (map-new))
+    ;; <vector>) ;; fixme
     ))
-(define macro-environment-get-transformer
-  (lambda (self car-expr)
+(define (macro-environment-id self) (vector-ref self 3))
+(define (macro-environment-parent self) (vector-ref self 4))
+(define (macro-environment-parent= self v)
+  (vector-set! self 4 v) self)
+(define (macro-environment-bindings self) (vector-ref self 5))
+(define (macro-environment-bindings= self v)
+  (vector-set! self 5 v) self)
+(define (macro-environment-write self port op)
+  (display "#<macro-environment " port)
+  (write (macro-environment-id self) port)
+  (display" >" port))
+; (%object-writer <macro-environment> macro-environment-write)
+(define (macro-environment-set-transformer self symbol transformer)
+  (map-set (macro-environment-bindings self) symbol transformer)
+  ;; (display "\n\n  ### set-macro ")(write symbol)(write transformer)(display "\n\n")
+  self)
+(define (macro-environment-get-transformer self car-expr)
     (let ((x (map-get (macro-environment-bindings self) car-expr)))
       (if (null? x)
 	(if (macro-environment-parent self)
 	  (macro-environment-get-transformer (macro-environment-parent self) car-expr)
 	  x)
-	x))))
-(define macro-environment-define-transformer
-  (lambda (self symbol transformer)
+	x)))
+(define (macro-environment-define-transformer self symbol transformer)
     (if (macro-environment-parent self)
       (macro-environment-define-transformer (macro-environment-parent self) symbol transformer)
-      (macro-environment-set-transformer self symbol transformer))))
-(define macro-environment-apply-transformer
-  (lambda (self transformer e)
+      (macro-environment-set-transformer self symbol transformer)))
+(define (macro-environment-apply-transformer self transformer e)
     ;; (display "  apply-macro ")(write transformer)(display " to ")(write e)(newline)
-    (apply transformer (cdr e))))
-(define macro-environment-expand-expr
-  (lambda (self e)
+    (apply transformer (cdr e)))
+(define (macro-environment-expand-expr self e)
     (if *macro-expand-trace*
       (let ()
 	(display "    expand-expr ")(write e)(newline)))
@@ -91,19 +75,17 @@
 		    (if (null? transformer)
 		      (macro-environment-expand-args self e)
 		      (macro-environment-apply-transformer self transformer e)))))))))
-      e)))
-(define macro-environment-expand-args
-  (lambda (self e)
+      e))
+(define (macro-environment-expand-args self e)
     (if (pair? e) 
       (let ()
 	;; (display "      expand-args ")(write (car e))(newline)
 	(cons 
 	  (macro-environment-expand-expr self (car e)) 
 	  (macro-environment-expand-args self (cdr e))))
-      e)))
+      e))
 
-(define macro-environment-expand
-  (lambda (self e)
+(define (macro-environment-expand self e)
     (let ((e-next (macro-environment-expand-expr self e)))
       (if (not (equal? e-next e))
         (let ()
@@ -117,12 +99,13 @@
           (if *macro-expand-trace*
             (let ()
               (display " result = ")(write e-next)(newline)))	  
-          e-next)))))
+          e-next))))
 
 (define *top-level-macro-environment* (macro-environment-new))
-(define &macro-environment (lambda () *top-level-macro-environment*))
-(define %define-macro (lambda (name transformer)
-  (macro-environment-define-transformer (&macro-environment) name transformer)))
+(define (&macro-environment) *top-level-macro-environment*)
+(define (%define-macro name transformer)
+  (macro-environment-define-transformer (&macro-environment) name transformer)
+  name)
 
 (%define-macro 'define-macro
   (lambda (name . body)
