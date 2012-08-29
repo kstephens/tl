ee785503988ce83fb3aa4ec25e4e21d8b293d6e5
Kurt Stephens <ks.github@kurtstephens.com>
2012-08-29 11:36:10 -0500

    Follow standards: http://www.cliki.net/naming%20conventions .

--- lib/tl/map.scm
+++ lib/tl/map.scm
@@ -1,5 +1,6 @@
 (define <map> (make-type "map"))
 (define (map-new) (cons '() '()))
+(define make-map map-new)
 (define (map-lookup self key) (assq key (car self)))
 (define (map-get self key)
   (let ((slot (map-lookup self key)))
@@ -10,5 +11,7 @@
       (set-cdr! slot value)
       (set-car! self (cons (cons key value) (car self)))
       )))
+(define (map-delete self key)
+  (error "not implemented"))
 
 'ok
