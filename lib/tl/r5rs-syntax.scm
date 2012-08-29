0b443c51614deddb64d3a3141452c82d9059146f
Kurt Stephens <ks.github@kurtstephens.com>
2012-08-29 13:41:40 -0500

    (let ...) suffices here: defines are transformed to set!, anyway.

--- lib/tl/r5rs-syntax.scm
+++ lib/tl/r5rs-syntax.scm
@@ -77,8 +77,8 @@
           (stmts (cdr defines-and-stmts)))
     (if (null? defines)
       (cons 'begin b)
-      `(begin (letrec ,(reverse defines) ,@(reverse stmts))))))
-        
+      `(begin (let ,(reverse defines) ,@(reverse stmts))))))
+
 (define-macro (case val-expr . cases)
   (letrec ((val (make-symbol #f))
 	   (%case 
