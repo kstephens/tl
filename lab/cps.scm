1a1c4b417f46c01f53ea8d61f1724e3d04c25c93
Kurt Stephens <ks.github@kurtstephens.com>
2012-09-11 10:09:11 -0500

    Prelim CPS using (match ...).

--- lab/cps.scm
+++ lab/cps.scm
@@ -1,43 +1,36 @@
+;; http://matt.might.net/articles/cps-conversion/
+(load "tl/match")
+
 (define genusym gensym)
 
 (define (T-k expr k)
-  (cond
-    ((and (pair? expr) (eq? 'lambda (car expr)))
-      (k (M expr)))
-    ((symbol? expr)
-      (k (M expr)))
-    ((pair? expr)
-      (let* (($rv (genusym '$rv))
-              (cont `(x (,$rv) ,(k $rv))))
-        (T-k f (lambda ($f)
-                 (T-K e (lambda ($e)
-                          `(,$f ,$e ,cont))))))))
-    )
-       
+  (match expr
+    [`(λ . ,_)      (k (M expr))]
+    [ (? symbol?)   (k (M expr))]
+    [`(,f ,e)    
+      ; =>
+      (define $rv (gensym '$rv))
+      (define cont `(λ (,$rv) ,(k $rv)))
+      (T-k f (λ ($f)
+             (T-k e (λ ($e)
+                      `(,$f ,$e ,cont)))))]))
+
 (define (T-c expr c)
-  (cond
-    ((and
-       (pair? expr)
-       (eq? 'lambda (car expr)))
-      `(,c ,(M expr)))
-    ((symbol? expr)
-      `(,c ,(M expr)))
-    ((pair? expr)
-      (T-k f (lambda ($f)
-               (T-k e (lambda ($e)
-                        `(,$f ,$e ,c))))))
-    ))
+  (match expr
+    [`(λ . ,_)     `(,c ,(M expr))]
+    [ (? symbol?)  `(,c ,(M expr))]
+    [`(,f ,e)    
+      ; =>
+      (T-k f (λ ($f)
+               (T-k e (λ ($e)
+                        `(,$f ,$e ,c)))))]))
      
 (define (M expr)
-  (cond
-    ((and
-       (pair? expr)
-       (eq? 'lambda (car expr)))
-      (let ((var (cadr expr))
-             (expr (caddr expr)))
-        (let (($k (genksym '$k)))
-          `(lambda (,var ,$k)
-             ,(T-c expr $k)))))
-    ((symbol? expr)
-      expr)
-    ))
+  (match expr
+    [`(λ (,var) ,expr)
+      ; =>
+      (define $k (gensym '$k))
+     `(λ (,var ,$k) ,(T-c expr $k))]
+    
+    [(? symbol?)  #;=>  expr]))
+     
