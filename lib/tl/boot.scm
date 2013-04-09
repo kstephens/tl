c7411254162298d25fc72e52eddcc52704a13ce3
Kurt Stephens <ks.github@kurtstephens.com>
2013-04-08 20:56:07 -0500

    Handle fullpath in load.

--- lib/tl/boot.scm
+++ lib/tl/boot.scm
@@ -394,7 +394,7 @@
     (if *load-verbose* (begin (display "load?: ")(display name)(newline)))
     (if (not (string-ends-with? name ".scm"))
       (set! name (string-append name ".scm")))
-    (set! pathname (locate-file-in-path name (cons (path-directory current-file) *load-path*)))
+    (set! pathname (locate-file name (cons (path-directory current-file) *load-path*)))
     (if (not pathname) (error "cannot locate" 'name name 'path *load-path*))
     (let ((in (open-file pathname "r"))
            (result #f))
