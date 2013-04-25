625f7adfbf5ea98bd972ddce927d5bdcb3c41201
Kurt Stephens <ks.github@kurtstephens.com>
2013-04-25 15:16:17 -0500

    Moved string-copy! to tl/string.scm.

--- lab/matt.might.net/scheme-to-c.scm
+++ lab/matt.might.net/scheme-to-c.scm
@@ -83,12 +83,6 @@
 
 
 ;; Utilities.
-(define (string-copy! to at from start end)
-  (if (and (< at (string-length to)) (< start end))
-    (begin
-      (string-set! to at (string-ref from start))
-      (string-copy! to (+ at 1) from (+ start 1) end)
-      )))
 (define (%string-truncate! s l)
   (let ((t (make-string l)))
     (string-copy! t 0 s 0 l)
