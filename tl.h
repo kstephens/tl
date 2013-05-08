de817e31b39a0a7de763f872d7b59c14c2a82fad
Kurt Stephens <ks.github@kurtstephens.com>
2013-05-08 16:37:42 -0500

    Clean up box/unbox functions.

--- tl.h
+++ tl.h
@@ -36,17 +36,21 @@ static inline tl tl_i(tlsw x) { return _tl_i(x); }
 static inline tlsw tl_I(tl o) { return _tl_I(o); }
 #define tl_I(o)_tl_I(o)
 
-#define tl_t_(o) ((tl*)(o))[-1]
-
-#define _tl_type(o)                           \
-  (                                           \
-   (o) == 0         ? tl_t_null :             \
-   ((tlw) (o)) & 1  ? tl_t_fixnum :           \
-   (o) <= tl_t      ? tl_t_boolean : tl_t_(o) )
-
+static inline tl tl_iv(tl o, int n) { return ((tl*)(o))[n]; }
 #define tl_iv(o,n) ((tl*)(o))[n]
 
-#define tl_FP(o,r,p) ((r(*)p)tl_iv(o, 0))
-#define tl_ENV(o) tl_iv(o,2)
+static inline tl tl_t_(tl o) { return tl_iv(o, -1); }
+#define tl_t_(o) tl_iv(o, -1)
+
+#define _tl_type(o)                             \
+  (                                             \
+   (o) == 0         ? tl_t_null :               \
+   ((tlw) (o)) & 1  ? tl_t_fixnum :             \
+   (o) <= tl_t      ? tl_t_boolean :            \
+                      tl_t_(o) )
+
+#define tl_FP(o,r,p) ((r(*)p) tl_iv(o, 0))
+
+static inline tl tl_closure_env(tl o) { return tl_iv(o, 2); }
 
 #endif
