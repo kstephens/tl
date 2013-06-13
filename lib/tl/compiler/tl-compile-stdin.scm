e98d142138355726ff37d0bf6bee800b65fa22fb
Kurt Stephens <ks.github@kurtstephens.com>
2013-06-13 00:12:59 -0500

    Refactor read-list.

--- lib/tl/compiler/tl-compile-stdin.scm
+++ lib/tl/compiler/tl-compile-stdin.scm
@@ -1,15 +1,5 @@
 (load "tl-to-c.scm")
-
-(define (read-list port)
-  (define (r a)
-    (let ((x (read port)))
-      (if (not (eof-object? x))
-        (begin
-          (set-cdr! a (cons x '()))
-          (r (cdr a))))))
-  (define l (list #f))
-  (r l)
-  (cdr l))
+(load "read-list.scm")
 
 (let ((the-program (cons 'begin (read-list tl_stdin))))
   ;; (tl_eval_debug #t)
