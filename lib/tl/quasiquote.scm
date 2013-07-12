c05956137b2a3d160a775c431e3baae10f2d872a
Kurt Stephens <ks.github@kurtstephens.com>
2013-07-11 21:25:33 -0500

    Identation makes tail-position code duplication obvious.

--- lib/tl/quasiquote.scm
+++ lib/tl/quasiquote.scm
@@ -2,30 +2,24 @@
 (define (%qq o l)
   (if (pair? o)
     (let ((l1
-            (if (eq? (car o) 'quasiquote)
-              (+ l 1)
-              (if (eq? (car o) 'unquote)
-                (- l 1)
-                (if (eq? (car o) 'unquote-splicing)
-                  (- l 1)
+            (if (eq? (car o) 'quasiquote)           (+ l 1)
+              (if (eq? (car o) 'unquote)            (- l 1)
+                (if (eq? (car o) 'unquote-splicing) (- l 1)
                   l)))))
       (if (= l 0)
         (if (eq? (car o) 'unquote)
           (cadr o)
           (if (pair? (car o))
             (if (eq? (caar o) 'unquote-splicing)
-              (list 'append (cadar o) (%qq (cdr o) l))
-              (list 'cons (%qq (car o) l1) (%qq (cdr o) l1)))
-            (list 'cons (%qq (car o) l1) (%qq (cdr o) l1))))
-        (list 'cons (%qq (car o) l1) (%qq (cdr o) l1))))
+              (list 'append (cadar o)        (%qq (cdr o) l ))
+              (list 'cons   (%qq (car o) l1) (%qq (cdr o) l1)))
+            (list   'cons   (%qq (car o) l1) (%qq (cdr o) l1))))
+        (list       'cons   (%qq (car o) l1) (%qq (cdr o) l1))))
     (if (vector? o)
       (list 'list->vector (%qq (vector->list o) l))
-      (if (number? o)
-        o
-        (if (string? o)
-          o
-          (if (char? o)
-            o
+      (if (number? o)    o
+        (if (string? o)  o
+          (if (char? o)  o
             (list 'quote o)))))))
 (define (%quasiquote expr)
   (if *quasiquote-debug*
